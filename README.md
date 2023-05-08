1725aa10c68172c11e07cba8cc1220d7d4350d09
sravikam <113577470+sravikam@users.noreply.github.com>
2023-05-08 22:57:56 +0530

    Functional

--- README.md
+++ README.md
@@ -3,18 +3,15 @@
 This was our submission for our college assignment in the subject of Parallel 
 Computer Architecture Programming. This project uses the stb_image library 
 files to read an image and then uses CUDA to parallely find average of both 
-images at each pixel.
+images at each pixel and outputs it to a new image file.
 
 ## How To Use
 
 ```bash
 # Compile the code using the nvcc library
 nvcc merge.cu -o merge
-```
 
-Note: All files must have the same extension
-
-
-```bash
-./merge image1.png image2.png result
+# Execute the file
+# ./merge image1.png image2.png name
+./merge images/rr.jpg images/cuteCat.png output
 ```
--- merge.cu
+++ merge.cu
@@ -3,8 +3,14 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define CHANNELS 3
 
 typedef unsigned char uchar;
 
@@ -15,40 +21,79 @@ __global__ void merge_images(uchar* dest, uchar* src1, int width1,
 	int y = blockIdx.y * blockDim.y + threadIdx.y;
 
 	if (x < width1 && y < height1) {
-		int index1 = y * width1 + x;
-		int index2 = (y * height2 / height1) * width2 + (x * width2 / width1);
+		int index1 = (y * width1 + x)*CHANNELS;
+		int index2 = ((y * height2 / height1) * width2 + (x * width2 / width1))*CHANNELS;
 
-		dest[index1 * 3 + 0] = (src1[index1 * 3 + 0] + src2[index2 * 3 + 0]) / 2;
-		dest[index1 * 3 + 1] = (src1[index1 * 3 + 1] + src2[index2 * 3 + 1]) / 2;
-		dest[index1 * 3 + 2] = (src1[index1 * 3 + 2] + src2[index2 * 3 + 2]) / 2;
+		for(int j=0; j<CHANNELS; ++j){
+			dest[index1 + j] = (src1[index1 + j] + src2[index2 + j]) / 2;
+		}
 	}
+}
+
+// Returns length if valid or 0 if invalid
+int isValidFilename(const char* filename){
+	const char* invalidChars = "\\/:*?\"<>|";
+	int len = strlen(filename);
+	for (int i = 0; i < len; ++i) {
+		if (strchr(invalidChars, filename[i]) != NULL) {
+			return 0;
+		}
+	}
+	return len;
 }
 
 int main(int argc, char* argv[]){
 
-	if(argc != 3){
-		fprintf(stderr, "Usage: ./merge inputFile1.png inputFile2.png");
+	if(argc < 3 || argc > 4){
+		fprintf(stderr, "Usage: ./merge inputFile1.png inputFile2.png [outputFile]");
 		exit(1);
 	}
 
-	int width1, height1, channels1;
+	char* outputFileName;
+	
+	if(argc == 4){
+		if(strrchr(argv[3], '.')){
+			fprintf(stderr, "Error: Output file should not be given an extension\n");
+			exit(1);
+		}
+		int value = isValidFilename(argv[3]);
+		if(value){
+			outputFileName = (char*)calloc(value+5, sizeof*outputFileName);
+			strcpy(outputFileName, argv[3]);
+			strcat(outputFileName, ".png");
+		} else{
+			fprintf(stderr, "Error: Invalid output file name");
+		}
+	} else{
+		outputFileName = (char*)calloc(11, sizeof*outputFileName);
+		strcpy(outputFileName, "result.png");
+	}
+	
 
-	uchar *image1 = stbi_load(argv[1], &width1, &height1, &channels1, STBI_rgb);
+	int width1, height1;
+	uchar *image1 = stbi_load(argv[1], &width1, &height1, NULL, STBI_rgb);
+	if(!image1){
+		fprintf(stderr, "Error: %s\n", stbi_failure_reason());
+		exit(1);
+	}
 
-	int width2, height2, channels2;
-
-	uchar *image2 = stbi_load(argv[2], &width2, &height2, &channels2, STBI_rgb);
+	int width2, height2;
+	uchar *image2 = stbi_load(argv[2], &width2, &height2, NULL, STBI_rgb);
+	if(!image2){
+		fprintf(stderr, "Error: %s\n", stbi_failure_reason());
+		exit(1);
+	}
 
 	// Allocate memory on the device
 	uchar *dev_dest, *dev_src1, *dev_src2;
 
-	cudaMalloc((void**)&dev_dest, width1 * height1 * 3);
-	cudaMalloc((void**)&dev_src1, width1 * height1 * 3);
-	cudaMalloc((void**)&dev_src2, width2 * height2 * 4);
+	cudaMalloc((void**)&dev_dest, width1 * height1 * CHANNELS);
+	cudaMalloc((void**)&dev_src1, width1 * height1 * CHANNELS);
+	cudaMalloc((void**)&dev_src2, width2 * height2 * CHANNELS);
 
 	// Copy the input images to the device
-	cudaMemcpy(dev_src1, image1, width1 * height1 * 3, cudaMemcpyHostToDevice);
-	cudaMemcpy(dev_src2, image2, width2 * height2 * 3, cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_src1, image1, width1 * height1 * CHANNELS, cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_src2, image2, width2 * height2 * CHANNELS, cudaMemcpyHostToDevice);
 
 	// Define block and grid sizes
 	dim3 blockSize(16, 16);
@@ -58,16 +103,17 @@ int main(int argc, char* argv[]){
 	merge_images<<<gridSize, blockSize>>>(dev_dest, dev_src1, width1, height1, dev_src2, width2, height2);
 
 	// Copy the result back to the host
-	uchar *result = (uchar*)malloc(width1 * height1 * 3);
-	cudaMemcpy(result, dev_dest, width1 * height1 * 3, cudaMemcpyDeviceToHost);
+	uchar *result = (uchar*)malloc(width1 * height1 * CHANNELS);
+	cudaMemcpy(result, dev_dest, width1 * height1 * CHANNELS, cudaMemcpyDeviceToHost);
 
 	// Save the result to disk
-	stbi_write_png("result.jpg", width1, height1, 3, result, width1 * 3);
+	stbi_write_png(outputFileName, width1, height1, CHANNELS, result, width1 * CHANNELS);
 
 	// Free memory on the host and device
 	free(result);
-	stbi_image_free(image1);
-	stbi_image_free(image2);
+	free(image1);
+	free(image2);
+	free(outputFileName);
 	cudaFree(dev_dest);
 	cudaFree(dev_src1);
 	cudaFree(dev_src2);
