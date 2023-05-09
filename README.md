0ff6eb2f7188b0688ffdbf1544b0dcdbf997057b
sravikam <113577470+sravikam@users.noreply.github.com>
2023-05-09 11:37:32 +0530

    Mosaic functional. Updated README

--- README.md
+++ README.md
@@ -2,16 +2,31 @@
 
 This was our submission for our college assignment in the subject of Parallel 
 Computer Architecture Programming. This project uses the stb_image library 
-files to read an image and then uses CUDA to parallely find average of both 
-images at each pixel and outputs it to a new image file.
+files to read an image and write an image.
+
+*merge*: Merges two images. The output image is of same size as first image.
+
+*mosaic*: Merges pixels of the same image to form a 32x32 tiled image.
 
 ## How To Use
 
 ```bash
 # Compile the code using the nvcc library
 nvcc merge.cu -o merge
+nvcc mosaic.cu -o mosaic
+```
 
-# Execute the file
-# ./merge image1.png image2.png name
-./merge images/rr.jpg images/cuteCat.png output
+### Execution
+
+```bash
+# To merge images/rr.jpg and images/cuteCat.png to an output image called 
+# merge_output.png, type the following code
+./merge images/rr.jpg images/cuteCat.png merge_output
+```
+
+```bash
+# To create a mosaic tiled image of images/rr.jpg
+./mosaic images/rr.jpg
+
+# Note: The output file will have the default name of result.png
 ```
--- mosaic.cu
+++ mosaic.cu
@@ -14,8 +14,37 @@
 
 typedef unsigned char uchar;
 
-__global__ uchar mosaicImage(uchar* dest, uchar* src, int width, int height){
-	
+// Define the kernel that will calculate the average of each tile
+__global__ void mosaicImage(uchar* outputImage, uchar* inputImage, int width, int height, int tileSize) {
+	int row = blockIdx.y;
+	int col = blockIdx.x;
+
+	int startRow = row * tileSize;
+	int startCol = col * tileSize;
+
+	int endRow = startRow + tileSize;
+	int endCol = startCol + tileSize;
+
+	if (endRow > height) endRow = height;
+	if (endCol > width) endCol = width;
+
+	long long total = 0;
+	for (int r = startRow; r < endRow; r++) {
+		for (int c = startCol; c < endCol; c++) {
+			int index = (r * width + c) * CHANNELS + threadIdx.x;
+			total += inputImage[index];
+		}
+	}
+
+	int tilePixels = (endRow - startRow) * (endCol - startCol);
+	uchar average = (uchar)(total / tilePixels);
+
+	for (int r = startRow; r < endRow; r++) {
+		for (int c = startCol; c < endCol; c++) {
+			int index = (r * width + c) * CHANNELS + threadIdx.x;
+			outputImage[index] = average;
+		}
+	}
 }
 
 // Returns length if valid or 0 if invalid
@@ -31,16 +60,13 @@ int isValidFilename(const char* filename){
 }
 
 int main(int argc, char* argv[]){
-	fprintf(stderr, "The kernel code has not been written!\n");
-	exit(0);
 
 	if(argc < 2 || argc > 3){
-		fprintf(stderr, "Usage: ./merge inputFile.png [outputFile]");
+		fprintf(stderr, "Usage: ./mosaic inputFile.png [outputFile]");
 		exit(1);
 	}
 
 	char* outputFileName;
-	
 	if(argc == 3){
 		if(strrchr(argv[2], '.')){
 			fprintf(stderr, "Error: Output file should not be given an extension\n");
@@ -58,7 +84,6 @@ int main(int argc, char* argv[]){
 		outputFileName = (char*)calloc(11, sizeof*outputFileName);
 		strcpy(outputFileName, "result.png");
 	}
-	
 
 	int width, height;
 	uchar *image = stbi_load(argv[1], &width, &height, NULL, STBI_rgb);
@@ -67,30 +92,28 @@ int main(int argc, char* argv[]){
 		exit(1);
 	}
 
-	// Allocate memory on the device
+	int smallerDimension = (width < height)? width:height;
+	int tileSize = smallerDimension>>5;
+
+	int numTilesX = (width + tileSize - 1) / tileSize;
+	int numTilesY = (height + tileSize - 1) / tileSize;
+
 	uchar *dev_dest, *dev_src;
 
-	cudaMalloc((void**)&dev_dest, width * height * CHANNELS);
-	cudaMalloc((void**)&dev_src, width * height * CHANNELS);
+	cudaMalloc(&dev_dest, width * height * CHANNELS);
+	cudaMalloc(&dev_src, width * height * CHANNELS);
 
-	// Copy the input images to the device
 	cudaMemcpy(dev_src, image, width * height * CHANNELS, cudaMemcpyHostToDevice);
 
-	// Define block and grid sizes
-	dim3 blockSize(16, 16);
-	dim3 gridSize((width + blockSize.x - 1) / blockSize.x, (height + blockSize.y - 1) / blockSize.y);
+	dim3 threadsPerBlock(CHANNELS);
+	dim3 numBlocks(numTilesX, numTilesY);
+	mosaicImage<<<numBlocks, threadsPerBlock>>>(dev_dest, dev_src, width, height, tileSize);
 
-	// Call the kernel to merge the images
-	mosaicImage<<<gridSize, blockSize>>>(dev_dest, dev_src, width, height);
-
-	// Copy the result back to the host
 	uchar *result = (uchar*)malloc(width * height * CHANNELS);
 	cudaMemcpy(result, dev_dest, width * height * CHANNELS, cudaMemcpyDeviceToHost);
 
-	// Save the result to disk
 	stbi_write_png(outputFileName, width, height, CHANNELS, result, width * CHANNELS);
 
-	// Free memory on the host and device
 	free(result);
 	free(image);
 	free(outputFileName);
